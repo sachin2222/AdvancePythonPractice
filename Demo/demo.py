14db42d0541339f5a9a503894a23ee1ceef502ce
sachin2222 <sachin.vashishth007@gmail.com>
2021-03-29 15:52:38 +0530

    python collections Practice

---
+++ Demo/demo.py
@@ -0,0 +1,45 @@
+a = 10
+print(type(a))
+
+b = 10.234
+print(type(b))
+
+c = 10.32j
+print(type(c))
+
+d = 10 > 90
+print(d)
+
+str1 = "Python Tester"
+print(len(str1))
+print(str1[-1])
+
+myList = ["sachin", 10, 20, 30, "sharma", "sachin"]
+print(myList)
+myList.reverse()
+print(myList)
+print(myList[0])
+myList.remove("sachin")
+print(myList)
+myList.append("Python-Automation Tester")
+print(myList)
+myList.insert(0, "Insert Testing")
+print(myList)
+
+dict = {1: "Python", 2: "java", 3: "c++", 4: "Ruby"}
+print(dict)
+print(dict[3])
+dict[1] = "Machine Learning"
+print(dict)
+
+tup = ("Tiger", "Lion", "Lion", "goat")
+print(tup)
+
+mySet = {10, 20, "sachin", 20}
+print(mySet)
+
+x = 10
+y = "Ten"
+print(str(x) + y)
+z = 10.23
+print(x + z)
